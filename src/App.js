0f51711e91b2f7204b5b0e74b6052280a029924c
Pio Molina <vpio@comcast.net>
2019-07-09 20:47:32 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const SNIPPETS = [
+    'Bears, beets, battlestar galactica',
+    "What's Forrest Gump's password? 1Forrest1",
+    'Where do programmers like to hangout? The Foo Bar'
+  ];
+
+  const INITIAL_GAME_STATE = { victory: false, startTime: null, endTime: null };
+  const [snippet, setSnippet] = useState('')
+  const [userText, setUserText] = useState('')
+  const [gameState, setGameState] = useState(INITIAL_GAME_STATE);
+
+  const updateUserText = event => {
+    setUserText(event.target.value);
+    console.log(`current userText `, userText);
+
+    if (event.target.value === snippet) {
+      setGameState({
+        ...gameState,
+        victory: true,
+        endTime: new Date().getTime() - gameState.startTime
+      })
+    }
+  }
+
+  const chooseSnippet = snippetIndex => () => {
+    console.log('setSnippet', snippetIndex);
+    setSnippet(SNIPPETS[snippetIndex]);
+    setGameState({ ...gameState, startTime: new Date().getTime() });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Type Race</h2>
+      <hr />
+      <h3>Snippet</h3>
+      {snippet}
+      <h4>{gameState.victory ? `Done! 🎉 Time: ${gameState.endTime}` : null}</h4>
+      <input value={userText} onChange={updateUserText}/>
+      <hr />
+      {
+        SNIPPETS.map((SNIPPET, index) => {
+          return (
+            <button onClick={chooseSnippet(index)} key={index}>
+              {SNIPPET.substring(0, 10)}...
+            </button>
+          )
+        })
+      }
     </div>
   );
 }
